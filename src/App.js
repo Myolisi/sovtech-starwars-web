852bfaca7cc97848ab55422783b85f6ec648d4dd
myolisi <myolisi@ngubentombi.co.za>
2022-02-02 21:03:47 +0200

    Prepare redux

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import People from './components/People';
+import { BrowserRouter as Router, Route, Routes, Link as RouterLink } from 'react-router-dom';
+import Details from './components/Details';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<People />} />
+        <Route path="/people/:id" element={<Details />} />
+      </Routes>
+    </Router>
   );
 }
 
